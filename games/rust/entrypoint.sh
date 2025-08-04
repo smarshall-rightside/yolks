b31a9dd7763d9a360dac483622929ba22ffcf328
Steven <73080506+smarshall-rightside@users.noreply.github.com>
2025-08-04 18:35:05 +0100

    Update entrypoint.sh

--- games/rust/entrypoint.sh
+++ games/rust/entrypoint.sh
@@ -4,8 +4,7 @@ cd /home/container
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $(NF-2);exit}'`
 
-
-## if auto_update is not set or to 1 update
+# Auto update Rust if enabled
 if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" == "1" ]; then
 	./steamcmd/steamcmd.sh +force_install_dir /home/container +login anonymous +app_update 258550 +quit
 else
@@ -16,24 +15,34 @@ fi
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
+# --- Carbon Installation ---
 if [[ "${FRAMEWORK}" == "carbon" ]]; then
-    # Carbon: https://github.com/CarbonCommunity/Carbon.Core
     echo "Updating Carbon..."
-    curl -sSL "https://github.com/CarbonCommunity/Carbon.Core/releases/download/production_build/Carbon.Linux.Release.tar.gz" | tar zx
+
+    if [[ "${RUST_STAGING}" == "1" ]]; then
+        echo "Fetching Carbon (staging build)..."
+        curl -sSL "https://api.carbonmod.org/download/staging" -o carbon.zip
+    else
+        echo "Fetching Carbon (stable build)..."
+        curl -sSL "https://api.carbonmod.org/download/latest" -o carbon.zip
+    fi
+
+    unzip -o -q carbon.zip
+    rm carbon.zip
     echo "Done updating Carbon!"
 
     export DOORSTOP_ENABLED=1
     export DOORSTOP_TARGET_ASSEMBLY="$(pwd)/carbon/managed/Carbon.Preloader.dll"
     MODIFIED_STARTUP="LD_PRELOAD=$(pwd)/libdoorstop.so ${MODIFIED_STARTUP}"
 
+# --- Oxide Installation ---
 elif [[ "$OXIDE" == "1" ]] || [[ "${FRAMEWORK}" == "oxide" ]]; then
-    # Oxide: https://github.com/OxideMod/Oxide.Rust
     echo "Updating uMod..."
     curl -sSL "https://github.com/OxideMod/Oxide.Rust/releases/latest/download/Oxide.Rust-linux.zip" > umod.zip
     unzip -o -q umod.zip
     rm umod.zip
     echo "Done updating uMod!"
-# else Vanilla, do nothing
+# Else: Vanilla = Do nothing
 fi
 
 # Fix for Rust not starting
